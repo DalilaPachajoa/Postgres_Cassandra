1a4bb9e918123682ad288f6217606921a4332ab1
dalila <70618572+DalilaPachajoa@users.noreply.github.com>
2020-09-04 11:57:42 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
---1
+--1) Obtener un listado de los empleados del hotel, con todos sus datos.
 /*SELECT	* 
 FROM 	public.empleado 
 ORDER 	BY numreg ASC*/
 
---2
+--2) Obtener el nombre del jefe del servicio de "Restaurante".
 /*select	nombre 
 from 	public.empleado 
 join 	servicio on servicio.numreg = empleado.numreg 
 		and descripcion = 'restaurante'*/
 
---3
+--3) Obtener el nombre del jefe de "Jorge Alonso Alonso".
 /*with	t1 as(
 	select	descripcion 
 	from 	empleado 
@@ -21,19 +21,19 @@ join 	servicio on servicio.numreg = empleado.numreg
 		and sueldo is not null
 join	t1 on t1.descripcion = servicio.descripcion*/
 
---4
+--4) Obtener un listado de los empleados y los servicios a los que estan asignados, exclusivamente para aquellos que tengan algun servicio asignado.
 /*select	empleado.* 
 from	public.empleado
 join 	servicio on servicio.cods = empleado.cods*/
 
---5
+--5) Obtener el numero de habitacion, tipo y precio de las habitaciones que esten ocupadas en la actualidad (no tienen fecha de salida).
 /*SELECT	habitacion.numero, habitacion.tipo, precio
 FROM 	habitacion
 join	factura on factura.numero = habitacion.numero
 join	precio on precio.tipo = habitacion.tipo
 where	salida is null*/
 
---6
+--6) Obtener el nombre y apellidos del cliente o clientes que mas veces hayan estado en el hotel (no numero total de noches, sino estancias diferentes: una persona que ha estado tres veces diferentes de una noche en el hotel habra "estado" mas veces que otra persona que ha estado una vez durante tres noches)
 /*with ms as(
 	select	max(cc) as cc from(
 		select	count(*) as cc
@@ -51,7 +51,7 @@ having	count(*) = (select cc from ms)
 order	by count(*) desc
 */
 
---7
+--7) Obtener los datos del empleado o empleados que hayan limpiado todas las habitaciones del hotel.
 /*select	t1.numreg, t1.nombre, t1.incorporacion, t1.sueldo, t1.cods, count(*)
 from(
 	select	distinct empleado.*, habitacion.numero
@@ -64,7 +64,7 @@ from(
 group	by t1.numreg, t1.nombre, t1.incorporacion, t1.sueldo, t1.cods
 having	count(*) = (select count(*) as cc from habitacion)*/
 
---8
+--8) Obtener el listado de los clientes que ocupen o hayan ocupado alguna vez habitaciones de los dos tipos (individual y doble)
 /*select	dni, nombre, apellidos, domicilio, telefono
 from	(
 	SELECT	cliente.*, tipo, count(*)
@@ -77,14 +77,14 @@ from	(
 group	by dni, nombre, apellidos, domicilio, telefono
 having	count(*) = 2*/
 
---9
+--9) Obtener un listado de los proveedores cuyas facturas hayan sido aprobadas unicamente por el encargado y no por el responsable de un servicio.
 /*select	factura_prov.*
 from	proveedor
 join	factura_prov on	factura_prov.nif = proveedor.nif
 left	join servicio on factura_prov.numreg = servicio.numreg
 where	servicio.cods is null*/
 
---10 revisar porque los otros tienen datos null
+--10) Igualar el sueldo del empleado que menos cobra dentro del servicio de "restaurante" con el del empleado que mas cobra del mismo servicio.
 /*with mmm as(
 	select	min(sueldo) as mi, max(sueldo) as ma, s1.cods
 	from 	empleado 
@@ -96,7 +96,7 @@ set		sueldo = (select ma from mmm)
 where	emp1.sueldo = (select mi from mmm)
 		and cods = (select cods from mmm)*/
 
---11
+--11) Incrementar en un 10% el sueldo del empleado de "limpieza" que maÌs habitaciones haya limpiado.
 /*
 with cleaner as (
 	select	empleado.numreg, count(*) 
